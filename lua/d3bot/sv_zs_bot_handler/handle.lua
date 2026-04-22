c99273126b0094d3cdecf73f7fc92a1dd7b8816d
Kerk <matthew.jacobs.works@gmail.com>
2026-04-22 12:23:21 -0500

    Znemesis select closes spawn to target H

--- lua/d3bot/sv_zs_bot_handler/handle.lua
+++ lua/d3bot/sv_zs_bot_handler/handle.lua
@@ -47,7 +47,9 @@ hook.Add("Think", D3bot.BotHooksId.."🤔", function()
 				gamemode.Call("PlayerDeathThink", bot)
 				if (not bot.StartSpectating or bot.StartSpectating <= CurTime()) and not (GAMEMODE.RoundEnded or bot.Revive or bot.NextSpawnTime) and bot:GetObserverMode() ~= OBS_MODE_NONE then
 					if GAMEMODE:GetWaveActive() then
-						bot:RefreshDynamicSpawnPoint()
+						if not D3bot.TrySetNemesisSpawn(bot) then
+							bot:RefreshDynamicSpawnPoint()
+						end
 						bot:UnSpectateAndSpawn()
 					else
 						bot:ChangeToCrow()
