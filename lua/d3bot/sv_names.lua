e2a2e10a8aa384ad9ccdaba1f996c686395dccca
Kerk <matthew.jacobs.works@gmail.com>
2026-05-28 20:59:46 -0500

    Revert "bot Z-main system"
    This reverts commit ff6934a22cb1a9097b4fbcd72d715c1b5018cedc.

--- lua/d3bot/sv_names.lua
+++ lua/d3bot/sv_names.lua
@@ -1,5 +1,4 @@
 D3bot.Names = {"Bot"}
-D3bot.ZombieMainBotName = "Z-main"
 
 -- TODO: Make search path relative
 if D3bot.BotNameFile then
@@ -15,16 +14,8 @@ local function getUsernames()
 end
 
 local names = {}
-function D3bot.GetUsername(forcedName)
+function D3bot.GetUsername()
 	local usernames = getUsernames()
-	if forcedName then
-		if not usernames[forcedName] then return forcedName end
-		local number = 2
-		while usernames[forcedName.."("..number..")"] do
-			number = number + 1
-		end
-		return forcedName.."("..number..")"
-	end
 	
 	if #names == 0 then names = table.Copy(D3bot.Names) end
 	local name = table.remove(names, math.random(#names))
