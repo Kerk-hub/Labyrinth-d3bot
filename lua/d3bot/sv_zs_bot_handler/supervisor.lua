3ea683ffee3e1a6ffe56a935fd6ee47726e228f6
Kerk <matthew.jacobs.works@gmail.com>
2026-05-28 20:59:07 -0500

    Revert "Z-main(bot) skips nemesis assignment"
    This reverts commit cd6eb9f63f24104c61f3fd797620ff40c54fde89.

--- lua/d3bot/sv_zs_bot_handler/supervisor.lua
+++ lua/d3bot/sv_zs_bot_handler/supervisor.lua
@@ -108,12 +108,6 @@ function D3bot.RefreshZombieNemesisAssignments(forceRefresh)
 		humanLookup[human] = true
 	end
 
-	for _, bot in ipairs(D3bot.GetBots()) do
-		if D3bot.IsZombieMainBot(bot) and bot.D3bot_Mem then
-			bot.D3bot_Mem.NemesisTarget = nil
-		end
-	end
-
 	local zombieBots = {}
 	for _, bot in ipairs(D3bot.GetBots()) do
 		if IsValid(bot) and bot:Team() == TEAM_UNDEAD and bot:Alive() and not D3bot.IsZombieMainBot(bot) then
@@ -152,7 +146,7 @@ function D3bot.RefreshZombieNemesisAssignments(forceRefresh)
 				end
 			end
 
-			if bestBot and bestBot.D3bot_Mem and not D3bot.IsZombieMainBot(bestBot) then
+			if bestBot and bestBot.D3bot_Mem then
 				bestBot.D3bot_Mem.NemesisTarget = human
 				assignedHumans[human] = true
 			end
